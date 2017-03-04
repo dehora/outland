f37202c152f4b4dc611a88eb6c7018ed574b322d
dehora <bill@dehora.net>
2017-03-04 12:14:10 +0000

    Add a DropwizardTestSupport setup for API testing.

---
+++ feature-server/src/test/java/outland/feature/server/resources/ServerSuite.java
@@ -0,0 +1,56 @@
+package outland.feature.server.resources;
+
+import com.google.common.collect.Lists;
+import com.google.inject.Module;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Environment;
+import io.dropwizard.testing.DropwizardTestSupport;
+import io.dropwizard.testing.ResourceHelpers;
+import io.dropwizard.testing.junit.DropwizardAppRule;
+import java.util.List;
+import org.glassfish.jersey.client.ClientProperties;
+import org.glassfish.jersey.client.JerseyClient;
+import org.glassfish.jersey.client.JerseyClientBuilder;
+import org.junit.ClassRule;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import outland.feature.server.ServerConfiguration;
+import outland.feature.server.ServerMain;
+import outland.feature.server.ServerModule;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({HelloResourceTest.class})
+public class ServerSuite {
+
+  private static final String CONFIG_PATH =
+      ResourceHelpers.resourceFilePath("test-configuration.yaml");
+
+  @ClassRule
+  public static final DropwizardAppRule<ServerConfiguration> APP =
+      new DropwizardAppRule<ServerConfiguration>(
+          new DropwizardTestSupport<ServerConfiguration>(ServerMain.class, CONFIG_PATH) {
+            @Override public Application<ServerConfiguration> newApplication() {
+
+              return new ServerMain("outland.feature.server") {
+
+                @Override
+                protected List<Module> addModules(
+                    ServerConfiguration configuration, Environment environment) {
+                  return Lists.newArrayList(
+                      new ServerModule(configuration)
+                  );
+                }
+              };
+            }
+          }
+
+      ) {
+
+      };
+
+  static JerseyClient client() {
+    return new JerseyClientBuilder()
+        .property(ClientProperties.CONNECT_TIMEOUT, 1000)
+        .property(ClientProperties.READ_TIMEOUT, 2000).build();
+  }
+}
