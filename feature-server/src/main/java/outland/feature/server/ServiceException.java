4d81024399f4df5c3b9ca3efb13f50fdacf74052
dehora <bill@dehora.net>
2017-03-04 12:52:46 +0000

    Return API errors in problem JSON format.

---
+++ feature-server/src/main/java/outland/feature/server/ServiceException.java
@@ -0,0 +1,46 @@
+package outland.feature.server;
+
+public class ServiceException extends RuntimeException {
+
+  /**
+   * Throw an IllegalArgumentException if the argument is null.
+   *
+   * @param arg the object to
+   * @param message the exception message
+   * @return the object if not null
+   * @throws IllegalArgumentException if {@code obj} is {@code null}
+   */
+  public static <T> T throwIfNull(T arg, String message) {
+    if (arg == null) {
+      IllegalArgumentException cause = new IllegalArgumentException(message);
+      throw new ServiceException(Problem.argProblem(cause.getMessage(), ""), cause);
+    }
+    return arg;
+  }
+
+  private Problem problem;
+
+  /**
+   * @param problem the Problem detail
+   */
+  public ServiceException(Problem problem) {
+    super(problem.toMessage());
+    this.problem = problem;
+  }
+
+  /**
+   * @param problem the Problem detail
+   * @param cause the cause
+   */
+  public ServiceException(Problem problem, Throwable cause) {
+    super(problem.toMessage(), cause);
+    this.problem = problem;
+  }
+
+  /**
+   * @return the problem detail
+   */
+  public Problem problem() {
+    return problem;
+  }
+}
