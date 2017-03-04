70968a8802ef4489eb05fafc3e3723876f7a2e93
dehora <bill@dehora.net>
2017-03-04 21:29:13 +0000

    Marshal feature JSON to a feature proto.

---
+++ feature-server/src/main/java/outland/feature/server/features/FeatureSupport.java
@@ -0,0 +1,19 @@
+package outland.feature.server.features;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.util.JsonFormat;
+import outland.feature.proto.Feature;
+
+public class FeatureSupport {
+
+  public static Feature toFeature(String json) {
+
+    try {
+      Feature.Builder builder = Feature.newBuilder();
+      JsonFormat.parser().merge(json, builder);
+      return builder.build();
+    } catch (InvalidProtocolBufferException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
---
+++ feature-server/src/test/java/outland/feature/server/features/FeatureSupportTest.java
@@ -0,0 +1,34 @@
+package outland.feature.server.features;
+
+import com.google.protobuf.util.JsonFormat;
+import org.junit.Test;
+import outland.feature.proto.Feature;
+
+import static org.junit.Assert.*;
+
+public class FeatureSupportTest {
+
+  @Test
+  public void testToFeature() throws Exception {
+
+    Feature feature = Feature.newBuilder()
+        .setId("id1")
+        .setKey("key1")
+        .setAppId("app1")
+        .setDescription("desc1")
+        .setState(Feature.State.off)
+        .build();
+
+    final String json = JsonFormat.printer().print(feature);
+
+    final Feature feature1 = FeatureSupport.toFeature(json);
+
+    assertEquals(feature.getId(), feature1.getId());
+    assertEquals(feature.getKey(), feature1.getKey());
+    assertEquals(feature.getAppId(), feature1.getAppId());
+    assertEquals(feature.getDescription(), feature1.getDescription());
+    assertEquals(feature.getState(), feature1.getState());
+
+  }
+
+}
