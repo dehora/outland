0e9462b153db488a46167358b3c4ace5130e19b5
dehora <bill@dehora.net>
2017-03-05 14:07:12 +0000

    Client support classes for basic auth and oauth.

---
+++ feature-java/src/main/java/outland/feature/AuthorizationProvider.java
@@ -0,0 +1,24 @@
+package outland.feature;
+
+import java.util.Optional;
+
+/**
+ * Provides a authorization string that can be used to authorize requests against the server or an
+ * outland node. Acts as a {@link FunctionalInterface} and can be supplied as a lambda expression.
+ */
+@FunctionalInterface
+public interface AuthorizationProvider {
+
+  String SCOPE_UID = "uid";
+
+  /**
+   * Provide an authorization string that can be used to authorize requests against the server or an
+   * outland node.
+   *
+   * @param appId the app id for the feature request
+   * @param scope an authorization scope such as an OAuth scope
+   * @return a value suitable for use as an Authorization header value, or empty to suppress the
+   * authorization being set
+   */
+  Optional<Authorization> authorization(String appId, String scope);
+}
