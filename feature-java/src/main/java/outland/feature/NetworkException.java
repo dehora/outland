4f0fa5fc159486b127aa1a15ee7a8460dd7232dc
dehora <bill@dehora.net>
2017-03-05 13:59:16 +0000

    Add exceptions to signal http and network problems.

---
+++ feature-java/src/main/java/outland/feature/NetworkException.java
@@ -0,0 +1,23 @@
+package outland.feature;
+
+/**
+ * An exception representing a network level problem, such as inability to access the network, or
+ * make an outbound connection.
+ */
+public class NetworkException extends HttpException {
+
+  /**
+   * @param problem the Problem detail
+   */
+  public NetworkException(Problem problem) {
+    super(problem);
+  }
+
+  /**
+   * @param problem the Problem detail
+   * @param cause the cause
+   */
+  public NetworkException(Problem problem, Throwable cause) {
+    super(problem, cause);
+  }
+}
