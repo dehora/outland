5435215665d57d7c6fddf45919b0af1672bf4ce9
dehora <bill@dehora.net>
2017-03-05 18:37:18 +0000

    Add test storage classes for client.

---
+++ feature-java/src/test/java/outland/feature/FeatureStoreLocalNull.java
@@ -0,0 +1,23 @@
+package outland.feature;
+
+import outland.feature.proto.Feature;
+import outland.feature.proto.FeatureCollection;
+
+public class FeatureStoreLocalNull implements FeatureStoreLocal {
+
+  @Override public Void put(Feature feature) throws FeatureException {
+    return null;
+  }
+
+  @Override public FeatureCollection loadAll() throws FeatureException {
+    return null;
+  }
+
+  @Override public FeatureCollection findAll(String appId) throws FeatureException {
+    return null;
+  }
+
+  @Override public void close() throws FeatureException {
+
+  }
+}
