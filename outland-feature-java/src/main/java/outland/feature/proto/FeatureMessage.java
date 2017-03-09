0f7257c572f566607ae9a6bcf8d3c495a7280681
dehora <bill@dehora.net>
2017-03-09 16:59:46 +0000

    Updates generated protobuf files.

--- outland-feature-java/src/main/java/outland/feature/proto/FeatureMessage.java
+++ outland-feature-java/src/main/java/outland/feature/proto/FeatureMessage.java
@@ -49,21 +49,21 @@ public final class FeatureMessage {
   static {
     java.lang.String[] descriptorData = {
       "\n\routland.proto\022\007outland\"+\n\014FeatureOwner" +
-      "\022\014\n\004name\030\002 \001(\t\022\r\n\005email\030\003 \001(\t\"/\n\016Feature" +
-      "Version\022\014\n\004ulid\030\001 \001(\t\022\017\n\007logical\030\002 \001(\003\"\354" +
-      "\002\n\007Feature\022\n\n\002id\030\001 \001(\t\022\013\n\003key\030\002 \001(\t\022\r\n\005a" +
-      "ppId\030\003 \001(\t\022%\n\005state\030\004 \001(\0162\026.outland.Feat" +
-      "ure.State\022\023\n\013description\030\005 \001(\t\022\017\n\007create" +
-      "d\030\006 \001(\t\022\017\n\007updated\030\007 \001(\t\022$\n\005owner\030\010 \001(\0132" +
-      "\025.outland.FeatureOwner\0224\n\nproperties\030\t \003" +
-      "(\0132 .outland.Feature.PropertiesEntry\022(\n\007" +
-      "version\030\014 \001(\0132\027.outland.FeatureVersion\0321",
-      "\n\017PropertiesEntry\022\013\n\003key\030\001 \001(\t\022\r\n\005value\030" +
-      "\002 \001(\t:\0028\001\"\"\n\005State\022\010\n\004none\020\000\022\007\n\003off\020\001\022\006\n" +
-      "\002on\020\002\"Q\n\021FeatureCollection\022\014\n\004type\030\001 \001(\t" +
-      "\022\r\n\005appId\030\002 \001(\t\022\037\n\005items\030\003 \003(\0132\020.outland" +
-      ".FeatureB+\n\025outland.feature.protoB\016Featu" +
-      "reMessageH\001P\001b\006proto3"
+      "\022\014\n\004name\030\002 \001(\t\022\r\n\005email\030\003 \001(\t\"@\n\016Feature" +
+      "Version\022\n\n\002id\030\001 \001(\t\022\021\n\ttimestamp\030\002 \001(\003\022\017" +
+      "\n\007counter\030\003 \001(\003\"\354\002\n\007Feature\022\n\n\002id\030\001 \001(\t\022" +
+      "\013\n\003key\030\002 \001(\t\022\r\n\005appId\030\003 \001(\t\022%\n\005state\030\004 \001" +
+      "(\0162\026.outland.Feature.State\022\023\n\013descriptio" +
+      "n\030\005 \001(\t\022\017\n\007created\030\006 \001(\t\022\017\n\007updated\030\007 \001(" +
+      "\t\022$\n\005owner\030\010 \001(\0132\025.outland.FeatureOwner\022" +
+      "4\n\nproperties\030\t \003(\0132 .outland.Feature.Pr" +
+      "opertiesEntry\022(\n\007version\030\014 \001(\0132\027.outland",
+      ".FeatureVersion\0321\n\017PropertiesEntry\022\013\n\003ke" +
+      "y\030\001 \001(\t\022\r\n\005value\030\002 \001(\t:\0028\001\"\"\n\005State\022\010\n\004n" +
+      "one\020\000\022\007\n\003off\020\001\022\006\n\002on\020\002\"Q\n\021FeatureCollect" +
+      "ion\022\014\n\004type\030\001 \001(\t\022\r\n\005appId\030\002 \001(\t\022\037\n\005item" +
+      "s\030\003 \003(\0132\020.outland.FeatureB+\n\025outland.fea" +
+      "ture.protoB\016FeatureMessageH\001P\001b\006proto3"
     };
     com.google.protobuf.Descriptors.FileDescriptor.InternalDescriptorAssigner assigner =
         new com.google.protobuf.Descriptors.FileDescriptor.    InternalDescriptorAssigner() {
@@ -88,7 +88,7 @@ public final class FeatureMessage {
     internal_static_outland_FeatureVersion_fieldAccessorTable = new
       com.google.protobuf.GeneratedMessageV3.FieldAccessorTable(
         internal_static_outland_FeatureVersion_descriptor,
-        new java.lang.String[] { "Ulid", "Logical", });
+        new java.lang.String[] { "Id", "Timestamp", "Counter", });
     internal_static_outland_Feature_descriptor =
       getDescriptor().getMessageTypes().get(2);
     internal_static_outland_Feature_fieldAccessorTable = new
