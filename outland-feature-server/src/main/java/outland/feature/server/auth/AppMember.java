c49b72d5a25c316b01d80a01b3b160f0d28ce1cd
dehora <bill@dehora.net>
2017-03-13 23:34:14 +0000

    Renames App auth object to AppMember.

---
+++ outland-feature-server/src/main/java/outland/feature/server/auth/AppMember.java
@@ -0,0 +1,51 @@
+package outland.feature.server.auth;
+
+import java.security.Principal;
+import java.util.List;
+
+public class AppMember implements Principal {
+
+  private String type;
+  private List<String> scopes;
+  private String identifier;
+  private String token;
+  private long expiresSeconds;
+
+  public AppMember(
+      String type,
+      String identifier,
+      List<String> scopes,
+      String token,
+      long expiresSeconds
+  ) {
+    this.type = type;
+    this.scopes = scopes;
+    this.identifier = identifier;
+    this.token = token;
+    this.expiresSeconds = expiresSeconds;
+  }
+
+  @Override public String getName() {
+    return identifier();
+  }
+
+  public String type() {
+    return type;
+  }
+
+  public List<String> scopes() {
+    return scopes;
+  }
+
+  public String identifier() {
+    return identifier;
+  }
+
+  public String token() { // not sure about this one
+    return token;
+  }
+
+  public long expiresSeconds() {
+    return expiresSeconds;
+  }
+}
