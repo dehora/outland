96e9fd1067bf8a1ebc348319ef7a464a3e013709
dehora <bill@dehora.net>
2017-03-14 16:26:00 +0000

    Externalises oauth remote server url.

--- outland-feature-server/src/main/java/outland/feature/server/auth/AuthModule.java
+++ outland-feature-server/src/main/java/outland/feature/server/auth/AuthModule.java
@@ -11,8 +11,12 @@ import java.net.URI;
 import java.util.concurrent.TimeUnit;
 import javax.inject.Inject;
 import okhttp3.OkHttpClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AuthModule extends AbstractModule {
+
+  private static final Logger logger = LoggerFactory.getLogger(AuthModule.class);
 
   private final AuthConfiguration authConfiguration;
 
@@ -25,19 +29,23 @@ public class AuthModule extends AbstractModule {
 
     bind(AuthConfiguration.class).toInstance(authConfiguration);
 
-    bind(new TypeLiteral<Authenticator<String, AuthPrincipal>>(){})
+    bind(new TypeLiteral<Authenticator<String, AuthPrincipal>>() {
+    })
         .annotatedWith(Names.named("oauthAppAuthenticator"))
         .to(TokenOAuthAuthenticator.class);
 
-    bind(new TypeLiteral<Authorizer<AuthPrincipal>>(){})
+    bind(new TypeLiteral<Authorizer<AuthPrincipal>>() {
+    })
         .annotatedWith(Names.named("oauthAppAuthorizer"))
         .to(TokenAuthorizer.class);
 
-    bind(new TypeLiteral<Authenticator<BasicCredentials, AuthPrincipal>>(){})
+    bind(new TypeLiteral<Authenticator<BasicCredentials, AuthPrincipal>>() {
+    })
         .annotatedWith(Names.named("basicAppAuthenticator"))
         .to(BasicAuthenticator.class);
 
-    bind(new TypeLiteral<Authorizer<AuthPrincipal>>(){})
+    bind(new TypeLiteral<Authorizer<AuthPrincipal>>() {
+    })
         .annotatedWith(Names.named("basicAppAuthorizer"))
         .to(BasicAuthorizer.class);
 
@@ -52,6 +60,9 @@ public class AuthModule extends AbstractModule {
     bind(OkHttpClient.class)
         .annotatedWith(Names.named("OAuthServiceClient"))
         .toInstance(client);
+
+    logger.info("op=configure_oauth,remote_oauth_lookup_url={}",
+        authConfiguration.remoteOAuthServer.tokenLookupURI);
 
     bind(URI.class)
         .annotatedWith(Names.named("OAuthServiceTokenLookupUri"))
