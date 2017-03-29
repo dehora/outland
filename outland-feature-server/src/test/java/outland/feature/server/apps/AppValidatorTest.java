75476352cbdc5ec38e6782c8575551323a47b57e
dehora <bill@dehora.net>
2017-03-29 13:30:11 +0100

    Adds an app validator checking min fields needed to create.

---
+++ outland-feature-server/src/test/java/outland/feature/server/apps/AppValidatorTest.java
@@ -0,0 +1,72 @@
+package outland.feature.server.apps;
+
+import org.junit.Test;
+import outland.feature.proto.App;
+import outland.feature.proto.Owner;
+import outland.feature.server.ServiceException;
+
+import static org.junit.Assert.*;
+
+public class AppValidatorTest {
+
+  @Test
+  public void validateAppRegistrationThrowingOk() {
+    App.Builder builder = App.newBuilder();
+    builder.addOwners(Owner.newBuilder().setName("Bob").setUsername("bob"))
+        .setKey("key1")
+        .setName("app 1");
+
+    try {
+      new AppValidator().validateAppRegistrationThrowing(builder.build());
+    } catch (ServiceException e) {
+      fail();
+    }
+  }
+
+  /*
+  these assume the order of validator checks so not that great. a better validator would
+  roll up all errors and throw just once
+   */
+
+  @Test
+  public void validateAppRegistrationThrowingNoOwner() {
+    App.Builder builder = App.newBuilder();
+    callValidate(builder, "no_owner_for_app", 422);
+  }
+
+  @Test
+  public void validateAppRegistrationThrowingJunkOwner() {
+    App.Builder builder = App.newBuilder();
+    builder.addOwners(Owner.newBuilder().setName("Bob"));
+
+    callValidate(builder, "incomplete_owner_for_app", 422);
+  }
+
+  @Test
+  public void validateAppRegistrationThrowingNoKey() {
+    App.Builder builder = App.newBuilder();
+    builder.addOwners(Owner.newBuilder().setName("Bob").setUsername("bob"));
+
+    callValidate(builder, "missing_key_for_app", 422);
+  }
+
+  @Test
+  public void validateAppRegistrationThrowingNoName() {
+    App.Builder builder = App.newBuilder();
+    builder.addOwners(Owner.newBuilder().setName("Bob").setUsername("bob"))
+        .setKey("key1");
+
+    callValidate(builder, "missing_name_for_app", 422);
+  }
+
+  private void callValidate(App.Builder builder, String expectedTitle, int expectedCode) {
+    try {
+      new AppValidator().validateAppRegistrationThrowing(builder.build());
+      fail();
+    } catch (ServiceException e) {
+      assertEquals(expectedCode, e.problem().status());
+      assertSame(expectedTitle, e.problem().title());
+    }
+  }
+
+}
