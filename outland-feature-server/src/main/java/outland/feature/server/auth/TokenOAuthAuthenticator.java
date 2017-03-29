fb6ac92e3fd697f6a564dead0937715f275b8aa9
dehora <bill@dehora.net>
2017-03-29 22:01:07 +0100

    Removes any oauth option other than a remote server check.

--- outland-feature-server/src/main/java/outland/feature/server/auth/TokenOAuthAuthenticator.java
+++ outland-feature-server/src/main/java/outland/feature/server/auth/TokenOAuthAuthenticator.java
@@ -8,34 +8,16 @@ import outland.feature.server.apps.AppAuthService;
 
 public class TokenOAuthAuthenticator implements io.dropwizard.auth.Authenticator<String, AuthPrincipal> {
 
-  private final AuthConfiguration serviceConfiguration;
   private final AppAuthService appAuthService;
 
   @Inject
   public TokenOAuthAuthenticator(
-      AuthConfiguration configuration,
       AppAuthService appAuthService
   ) {
-    this.serviceConfiguration = configuration;
     this.appAuthService = appAuthService;
   }
 
   @Override public Optional<AuthPrincipal> authenticate(String credentials) throws AuthenticationException {
-
-    if (AuthConfiguration.AUTHENTICATION_POLICY_OAUTH_BEARER_REFLECT.equals(
-        serviceConfiguration.oauthAuthenticationPolicy)) {
-      /*
-      Exercises oauth by sending bearer credentials in the form identity/[service,owner].
-      Exists for development.
-       */
-
-      final String[] split = credentials.split("/");
-
-      return Optional.of(new AuthPrincipal(
-          split[1], split[0], Lists.newArrayList(TokenAuthorizer.WILDCARD_SCOPE)
-      ));
-    }
-
     return appAuthService.authenticate(credentials, AppAuthService.BEARER);
   }
 }
