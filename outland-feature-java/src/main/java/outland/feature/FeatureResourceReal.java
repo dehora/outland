6ca4d5d217f0cb514d134d0a73cbc7ddb4411bc7
dehora <bill@dehora.net>
2017-04-05 21:09:44 +0100

    Renames App to Namespace.

--- outland-feature-java/src/main/java/outland/feature/FeatureResourceReal.java
+++ outland-feature-java/src/main/java/outland/feature/FeatureResourceReal.java
@@ -17,22 +17,22 @@ class FeatureResourceReal implements FeatureResource {
 
   private final ResourceProvider resourceProvider;
   private final AuthorizationProvider authorizationProvider;
-  private final String appKey;
+  private final String nsKey;
   private final URI baseUri;
-  private final boolean multiAppEnabled;
+  private final boolean multiNamespaceEnabled;
 
   FeatureResourceReal(
       ResourceProvider resourceProvider,
       AuthorizationProvider authorizationProvider,
-      String appKey,
+      String nsKey,
       URI baseUri,
-      boolean multiAppEnabled
+      boolean multiNamespaceEnabled
   ) {
     this.resourceProvider = resourceProvider;
     this.authorizationProvider = authorizationProvider;
-    this.appKey = appKey;
+    this.nsKey = nsKey;
     this.baseUri = baseUri;
-    this.multiAppEnabled = multiAppEnabled;
+    this.multiNamespaceEnabled = multiNamespaceEnabled;
   }
 
   @Override public Feature register(Feature feature) {
@@ -52,7 +52,7 @@ class FeatureResourceReal implements FeatureResource {
 
   @Override public Feature update(Feature feature) {
     FeatureException.throwIfNull(feature, "Please supply a feature");
-    FeatureException.throwIfNull(feature.getAppkey(), "Please add an app id to the feature");
+    FeatureException.throwIfNull(feature.getAppkey(), "Please add a namespace key to the feature");
     FeatureException.throwIfNull(feature.getKey(), "Please add a feature key to the feature");
 
     try {
@@ -70,103 +70,103 @@ class FeatureResourceReal implements FeatureResource {
   }
 
   @Override public Feature findByKey(String featureKey) {
-    if(multiAppEnabled) {
-      throw new FeatureException(Problem.configProblem("find_by_key_multi_with_no_appkey",
-          "Find by key cannot be called with only a feature key when multi app is configured. "
-              + "Please supply an app id as well as a feature for multi app configuration."));
+    if(multiNamespaceEnabled) {
+      throw new FeatureException(Problem.configProblem("find_by_key_multi_with_no_ns_key",
+          "Find by key cannot be called with only a feature key when multi namespace is configured. "
+              + "Please supply a namespace key as well as a feature for multi namespace configuration."));
     }
 
     FeatureException.throwIfNull(featureKey, "Please supply a featureKey");
 
-    return findByKey(appKey, featureKey);
+    return findByKey(nsKey, featureKey);
   }
 
   @Override public FeatureCollection listFeatures() {
-    if(multiAppEnabled) {
-      throw new FeatureException(Problem.configProblem("list_features_multi_with_no_appkey",
-          "List features cannot be called without an app id key when multi app is configured. "
-              + "Please supply an app id for multi app configuration."));
+    if(multiNamespaceEnabled) {
+      throw new FeatureException(Problem.configProblem("list_features_multi_with_no_ns_key",
+          "List features cannot be called without a namespace key when multi namespace is configured. "
+              + "Please supply a namespace key for multi namespace configuration."));
     }
 
-    return listFeatures(appKey);
+    return listFeatures(nsKey);
   }
 
   @Override public FeatureCollection listFeaturesSince(long timestamp, TimeUnit timeUnit) {
-    if(multiAppEnabled) {
-      throw new FeatureException(Problem.configProblem("list_features_since_multi_with_no_appkey",
-          "List features since cannot be called without an app id key when multi app is configured. "
-              + "Please supply an app id for multi app configuration."));
+    if(multiNamespaceEnabled) {
+      throw new FeatureException(Problem.configProblem("list_features_since_multi_with_no_ns_key",
+          "List features since cannot be called without a namespace key when multi namespace is configured. "
+              + "Please supply a namespace key for multi namespace configuration."));
     }
 
     FeatureException.throwIfNull(timeUnit, "Please supply a timeUnit");
 
-    return listFeaturesSince(appKey, timestamp, timeUnit);
+    return listFeaturesSince(nsKey, timestamp, timeUnit);
   }
 
   @Override
-  public Feature findByKey(String appKey, String featureKey) {
-    FeatureException.throwIfNull(appKey, "Please supply an appKey");
+  public Feature findByKey(String nsKey, String featureKey) {
+    FeatureException.throwIfNull(nsKey, "Please supply a nsKey");
     FeatureException.throwIfNull(featureKey, "Please supply a featureKey");
 
-    return findByKeyInner(appKey, featureKey);
+    return findByKeyInner(nsKey, featureKey);
   }
 
   @Override
-  public FeatureCollection listFeatures(String appKey) {
-    FeatureException.throwIfNull(appKey, "Please supply an appKey");
+  public FeatureCollection listFeatures(String nsKey) {
+    FeatureException.throwIfNull(nsKey, "Please supply a nsKey");
 
-    return listFeaturesInner(appKey);
+    return listFeaturesInner(nsKey);
   }
 
   @Override
-  public FeatureCollection listFeaturesSince(String appKey, long timestamp, TimeUnit timeUnit) {
-    FeatureException.throwIfNull(appKey, "Please supply an appKey");
+  public FeatureCollection listFeaturesSince(String nsKey, long timestamp, TimeUnit timeUnit) {
+    FeatureException.throwIfNull(nsKey, "Please supply a nsKey");
     FeatureException.throwIfNull(timeUnit, "Please supply a timeUnit");
 
-    return listFeaturesSinceInner(appKey, timestamp, timeUnit);
+    return listFeaturesSinceInner(nsKey, timestamp, timeUnit);
   }
 
-  private Feature findByKeyInner(String appKey, String key) {
+  private Feature findByKeyInner(String nsKey, String key) {
     try {
 
       String url = UriBuilder.builder(baseUri)
           .path(PATH_FEATURES)
-          .path(appKey)
+          .path(nsKey)
           .path(key)
           .buildString();
 
-      return toFeature(getRequest(appKey, url).body().string());
+      return toFeature(getRequest(nsKey, url).body().string());
     } catch (IOException e) {
       throw new FeatureException(Problem.localProblem(e.getMessage(), ""), e);
     }
   }
 
-  private FeatureCollection listFeaturesInner(String appKey) {
+  private FeatureCollection listFeaturesInner(String nsKey) {
     try {
 
       String url = UriBuilder.builder(baseUri)
           .path(PATH_FEATURES)
-          .path(appKey)
+          .path(nsKey)
           .buildString();
 
-      return toFeatureCollection(getRequest(appKey, url).body().string());
+      return toFeatureCollection(getRequest(nsKey, url).body().string());
     } catch (IOException e) {
       throw new FeatureException(Problem.localProblem(e.getMessage(), ""), e);
     }
   }
 
-  private FeatureCollection listFeaturesSinceInner(String appKey, long timestamp,
+  private FeatureCollection listFeaturesSinceInner(String nsKey, long timestamp,
       TimeUnit timeUnit) {
     try {
       long ts = timeUnit.toSeconds(timestamp);
 
       String url = UriBuilder.builder(baseUri)
           .path(PATH_FEATURES)
-          .path(appKey)
+          .path(nsKey)
           .query(PARAM_SINCE, ts + "")
           .buildString();
 
-      return toFeatureCollection(getRequest(appKey, url).body().string());
+      return toFeatureCollection(getRequest(nsKey, url).body().string());
     } catch (IOException e) {
       throw new FeatureException(Problem.localProblem(e.getMessage(), ""), e);
     }
@@ -184,10 +184,10 @@ class FeatureResourceReal implements FeatureResource {
     return builder.build();
   }
 
-  private Response getRequest(String appKey, String url) {
+  private Response getRequest(String nsKey, String url) {
     return resourceProvider
         .newResource()
-        .requestThrowing(Resource.GET, url, prepareOptions(appKey));
+        .requestThrowing(Resource.GET, url, prepareOptions(nsKey));
   }
 
   private Response postRequest(String url, Feature feature) {
@@ -196,9 +196,9 @@ class FeatureResourceReal implements FeatureResource {
         .requestThrowing(Resource.POST, url, prepareOptions(feature.getAppkey()), feature);
   }
 
-  private ResourceOptions prepareOptions(String appKey) {
+  private ResourceOptions prepareOptions(String nsKey) {
     return ResourceSupport.options(APPLICATION_JSON)
-        .appKey(appKey)
+        .appKey(nsKey)
         .securityTokenProvider(authorizationProvider);
   }
 }
