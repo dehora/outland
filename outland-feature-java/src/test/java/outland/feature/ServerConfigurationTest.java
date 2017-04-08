79954bda940f6deb0c95bb3be6f4ebf9be6f7d17
dehora <bill@dehora.net>
2017-04-08 14:50:01 +0100

    Adds client configuration tests.

--- outland-feature-java/src/test/java/outland/feature/ServerConfigurationTest.java
+++ outland-feature-java/src/test/java/outland/feature/ServerConfigurationTest.java
@@ -1,11 +1,32 @@
 package outland.feature;
 
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class ServerConfigurationTest {
+
+  @Test
+  public void testConfiguration() throws Exception {
+    ServerConfiguration configuration = new ServerConfiguration()
+        .baseURI(new URI("http://localhost"))
+        .connectTimeout(8, TimeUnit.SECONDS)
+        .readTimeout(7, TimeUnit.SECONDS)
+        .httpLoggingEnabled(true)
+        .certificatePath("foo")
+        .initialCacheSize(20)
+        .maxCacheSize(12)
+        ;
+    assertEquals(8000, configuration.connectTimeout());
+    assertEquals(7000, configuration.readTimeout());
+    assertEquals(true, configuration.httpLoggingEnabled());
+    assertEquals("foo", configuration.certificatePath());
+    assertEquals(20, configuration.initialCacheSize());
+    assertEquals(12, configuration.maxCacheSize());
+  }
 
   @Test
   public void testNamespaceConfiguration() {
