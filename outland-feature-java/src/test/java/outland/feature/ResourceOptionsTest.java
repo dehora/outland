f962837f9927e4516ca501519ad1a8469ae10455
dehora <bill@dehora.net>
2017-04-08 14:51:28 +0100

    Adds tests for client resource.

---
+++ outland-feature-java/src/test/java/outland/feature/ResourceOptionsTest.java
@@ -0,0 +1,49 @@
+package outland.feature;
+
+import com.google.common.collect.Maps;
+import java.util.HashMap;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ResourceOptionsTest {
+
+  @Test
+  public void testScope() {
+    ResourceOptions ro = new ResourceOptions();
+
+    try {
+      ro.scope(null);
+      fail();
+    } catch (Exception ignored) {
+    }
+
+    assertEquals("foo", ro.scope("foo").scope());
+  }
+
+  @Test
+  public void testHeaders() {
+    ResourceOptions ro = new ResourceOptions();
+
+    try {
+      ro.headers(null);
+      fail();
+    } catch (Exception ignored) {
+    }
+
+    final HashMap<String, Object> headers = Maps.newHashMap();
+    headers.put("Foo", "Bar");
+    ro.headers(headers).headers();
+    headers.put("Baz", "Quux");
+    ro.headers(headers).headers();
+    ro.header("A", "B");
+
+    final HashMap<String, Object> headers1 = Maps.newHashMap();
+    headers1.put("Foo", "Bar");
+    headers1.put("Baz", "Quux");
+    headers1.put("A", "B");
+
+    assertEquals(headers1, ro.headers());
+  }
+
+}
